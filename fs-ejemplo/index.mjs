86f6f401a70a936838d5d1e28c5a31f7b5b36966
sofiaguer0 <slaguero@institutosanmartin.edu.ar>
2025-09-20 18:52:19 -0300

    fs ejemplo

---
+++ fs-ejemplo/index.mjs
@@ -0,0 +1,18 @@
+import fs from "fs";
+
+// Leer un archhivo de manera asincrónica;
+fs.readFile("./data/example.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log("Contenido del archivo:", data);
+});
+
+// Escribir en un nuevo archivo;
+fs.writeFile("./data/newfile.txt", "Contenido nuevo", (err) => {
+  if (err) throw err;
+  console.log("Archivo creado y escrito");
+
+  fs.rename("./data/newfile.txt", "./data/renamedfile.txt", (err) => {
+    if (err) throw err;
+    console.log("Archivo renombrado");
+  });
+});
